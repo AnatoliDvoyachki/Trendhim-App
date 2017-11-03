253e14695ac7824e055505871cbf2bb869ed69b5
tolco97 <tolco97@abv.bg>
2017-11-03 11:25:26 +0100

    Download thread & stuff

--- TrendhimApp2/app/src/main/java/com/example/asus/trendhimapp/ProductPage/ProductActivity.java
+++ TrendhimApp2/app/src/main/java/com/example/asus/trendhimapp/ProductPage/ProductActivity.java
@@ -11,7 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.asus.trendhimapp.BaseActivity;
+import com.example.asus.trendhimapp.MainActivities.BaseActivity;
 import com.example.asus.trendhimapp.ProductPage.Products.*;
 import com.example.asus.trendhimapp.R;
 import com.google.firebase.database.DatabaseReference;
