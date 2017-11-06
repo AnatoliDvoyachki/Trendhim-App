9cb4368a384503eef06ad6e825c0ac2c996d9e39
tolco97 <tolco97@abv.bg>
2017-11-06 13:35:16 +0100

    WishlistPRoduct saving

--- TrendhimApp2/app/src/main/java/com/example/asus/trendhimapp/ProductPage/ProductActivity.java
+++ TrendhimApp2/app/src/main/java/com/example/asus/trendhimapp/ProductPage/ProductActivity.java
@@ -108,12 +108,15 @@ public class ProductActivity extends BaseActivity implements View.OnClickListene
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         Intent intent = getIntent();
         if (intent != null) {
-            WishlistProduct wishlistProduct;
             String userEmail = user.getEmail();
             String productKey = intent.getStringExtra("productKey");
-            String entityName = productKey.replaceAll("\\d", "s"); // replace digits with s
-            wishlistProduct = new WishlistProduct(productKey, entityName, userEmail);
-            myRef.push().setValue(wishlistProduct);
+            String entityName;
+            if (productKey.startsWith("watch")) {
+                entityName = productKey.replace("\\d", "es");
+            } else {
+                entityName = productKey.replace("\\d", "s");
+            }
+            myRef.push().setValue(new WishlistProduct(productKey, entityName, userEmail));
             Toast.makeText(this, "Item successfuly added to wishlist!", Toast.LENGTH_SHORT).show();
         }
     }
